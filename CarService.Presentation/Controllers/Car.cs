69593abfbad20ee3bc34b20ee6318cc1d6d60996
Xabartshik <danoshlakov12@gmail.com>
2025-06-24 15:00:48 +1000

    Добавьте файлы проекта.

---
+++ CarService.Presentation/Controllers/Car.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarService.Presentation.Controllers
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        [Required]
+        private string _brand;
+        public string Brand
+        {
+            get
+            {
+                return _brand;
+            }
+            set
+            {
+                value ??= "Unknown";
+                _brand = value.Trim() ?? "Unknown";
+            }
+        }
+        [Required]
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string? OwnerName { get; set; }
+    }
+}
